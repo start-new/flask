1bf8bd95e6f4fb3becb2f58fad600656c35f3b39
Thierry Chappuis <tchappui@users.noreply.github.com>
2020-02-04 14:25:47 +0100

    Update __main__.py

--- {{ cookiecutter.project_slug }}/{{ cookiecutter.project_app_package }}/__main__.py
+++ {{ cookiecutter.project_slug }}/{{ cookiecutter.project_app_package }}/__main__.py
@@ -1,7 +1,10 @@
+"""Main module of the application."""
+
 import os
 
 from . import create_app, db
 from flask_migrate import Migrate
 
-app = create_app(os.getenv('FLASK_CONFIG') or 'default')
-migrate = Migrate(app, db)
+if __name__ == "__main__":
+  app = create_app(os.getenv('FLASK_CONFIG') or 'default')
+  migrate = Migrate(app, db)
