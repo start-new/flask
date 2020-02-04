53d785548305a17a328c59ac59c3b4ed5ce5cf73
Thierry Chappuis <tchappui@users.noreply.github.com>
2020-02-04 14:58:02 +0100

    Update settings.py

--- {{ cookiecutter.project_slug }}/settings.py
+++ {{ cookiecutter.project_slug }}/settings.py
@@ -15,8 +15,9 @@ INSTALLED_BLUEPRINTS = [
 
 
 class Environment:
-    SECRET_KEY = os.getenv("SECRET_KEY")
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = os.environ.get("SECRET_KEY", "very secret and unguessable value") 
+    {%- if cookiecutter.use_flask_sqlalchemy == 'y' %}
+    SQLALCHEMY_TRACK_MODIFICATIONS = False{% endif %}
 
     @staticmethod
     def init_app(app):
@@ -25,21 +26,24 @@ class Environment:
 
 class DevelopmentEnvironment(Environment):
     DEBUG = True
+    {%- if cookiecutter.use_flask_sqlalchemy == 'y' %}
     SQLALCHEMY_DATABASE_URI = (
         os.getenv("DEV_DATABASE_URL") or f"sqlite:///{BASE_DIR / 'dev.db'}"
-    )
+    ){% endif %}
 
 
 class TestingEnvironment(Environment):
     TESTING = True
-    SQLALCHEMY_DATABASE_URI = os.getenv("TEST_DATABASE_URL") or "sqlite://"
+    {%- if cookiecutter.use_flask_sqlalchemy == 'y' %}
+    SQLALCHEMY_DATABASE_URI = os.getenv("TEST_DATABASE_URL") or "sqlite://"{% endif %}
 
 
 class ProductionEnvironment(Environment):
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    {%- if cookiecutter.use_flask_sqlalchemy == 'y' %}
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL"){% endif %}
 
 
-config = {
+environments = {
     "development": DevelopmentEnvironment,
     "testing": TestingEnvironment,
     "production": ProductionEnvironment,
